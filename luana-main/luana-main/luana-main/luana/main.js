6cae76e16e54582bd51e6f151267ec6e0376677a
luanarodriguex <103640240+luanarodriguex@users.noreply.github.com>
2024-05-17 16:46:14 -0300

    Add files via upload

---
+++ luana-main/luana-main/luana-main/luana/main.js
@@ -0,0 +1,8 @@
+textos[j].classList.remove(“ativo”);
+const botoes = document.querySelectorAll("botao")
+console.log(botoes)
+for(let i=0;<botoes.length;i++){
+
+}
+    botoes [I].onclick = function
+    for(let j=0;j<botoes)
